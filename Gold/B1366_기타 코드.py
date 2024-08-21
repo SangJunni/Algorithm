5b648c4072df140697d56b349a6366197dec52c9
SangJunni <yoonsj0322@gmail.com>
2024-08-21 19:17:32 +0900

    [Feat] 알고리즘 문제 풀이 업로드

--- Gold/B1366_기타 코드.py
+++ Gold/B1366_기타 코드.py
@@ -1,23 +1,23 @@
 from itertools import product
-n, m= map(int,input().split())
-tones=list(("A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"))
-current=list(input().split())
-code=list(input().split())
-answer=1e10
+n, m = map(int,input().split())
+tones = list(("A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"))
+current = list(input().split())
+code = list(input().split())
+answer = 1e10
 for goal in product(range(m),repeat=n):
     if len(set(goal)) != m:
         continue
     flats = []
     for k in range(n):
-        number=0
-        currenttone=tones.index(current[k])
-        if tones[currenttone]==code[goal[k]]:
+        number = 0
+        current_tone = tones.index(current[k])
+        if tones[current_tone] == code[goal[k]]:
             continue
-        while tones[(currenttone+number)%12]!=code[goal[k]]:
-            number+=1
+        while tones[(current_tone + number) % 12] != code[goal[k]]:
+            number += 1
         flats.append(number)
     if flats:
-        temp=[]
+        temp = []
         temp.append(max(flats)-min(flats)+1)
         flats.sort()
         for i in range(1, len(flats)):
